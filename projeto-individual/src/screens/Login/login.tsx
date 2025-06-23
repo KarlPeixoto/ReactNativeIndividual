7711276946a94721900f0affa672d6cd15d86726
Mateus Karl Peixoto <mateuskpeixoto@gmail.com>
2025-06-23 13:24:14 -0300

    Página Login completa

--- projeto-individual/src/screens/Login/login.tsx
+++ projeto-individual/src/screens/Login/login.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, TextInput } from 'react-native';
 import { styles } from './styles'
 import pokemon from '../../../assets/International_Pokémon_logo.svg.png'
+import eye from '../../../assets/eye.png'
 
 export function Login() {
     return (
         <View style={styles.container}>
             <Image style={styles.imagem} source={pokemon} />
             <Text style={styles.textoGrande}>Entre na sua conta do Clube de Treinadores de Pokemon</Text>
+            <TextInput style={styles.input} placeholder='Nome de usuário' placeholderTextColor='#9390AC' />
+            <TextInput 
+            style={styles.input} placeholder='Senha' placeholderTextColor='#9390AC' secureTextEntry={true}
+            />
+            <Image style={styles.eye} source={eye} />
+            <Text style={styles.textoProblema}>Problemas ao fazer o Login?</Text>
+            <TouchableOpacity>
+                <Text style={styles.buttonEntrar}>Entrar</Text>
+            </TouchableOpacity>
+            <Text style={styles.textoConta}>Você já tem uma conta?</Text>
+            <TouchableOpacity>
+                <Text style={styles.buttonCriar}>Criar conta</Text>
+            </TouchableOpacity>
+            <Text style={styles.textoPokemon1}>©2025 Pokémon.</Text>
+            <Text style={styles.textoPokemon2}>©1995 - 2025 Nintendo/Creatures Inc./GAME FREAK inc. TM, ®NINTENDO</Text>
         </View>
     )
 }
--- projeto-individual/src/screens/Login/styles.ts
+++ projeto-individual/src/screens/Login/styles.ts
@@ -6,17 +6,75 @@ export const styles = StyleSheet.create({
 
     },
     imagem: {
-    width: 300,
-    height: 110,
-    alignSelf: 'center',
-    marginTop: 100
-   },
-   textoGrande: {
-    fontSize: 25,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 20,
-    fontFamily: 'comic sans ms'
-   }
+        width: 300,
+        height: 110,
+        alignSelf: 'center',
+        marginTop: 100
+    },
+    textoGrande: {
+        fontSize: 25,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginTop: 20,
+        fontFamily: 'comic sans ms'
+    },
+    input: {
+        backgroundColor: '#252526',
+        borderRadius: 10,
+        margin: 10
+    },
+    eye: {
+        width: 30,
+        height: 30,
+        marginLeft: 330,
+        marginTop: -45,
+        tintColor: '#9390AC',
+    },
+    textoProblema: {
+        textAlign: 'center',
+        marginTop: 20,
+        color: '#049DD9'
+    },
+    buttonEntrar: {
+        backgroundColor: '#3FB047',
+        marginTop: 20,
+        borderRadius: 10,
+        width: 350,
+        height: 50,
+        alignSelf: 'center',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontWeight: 'bold',
+        fontSize: 15,
+        color: '#fff'
+    },
+    textoConta: {
+        textAlign: 'center',
+        marginTop: 50
+    },
+    buttonCriar: {
+        backgroundColor: '#fff',
+        marginTop: 20,
+        borderRadius: 10,
+        borderWidth: 2,
+        borderColor: '#3FB047',
+        width: 350,
+        height: 50,
+        alignSelf: 'center',
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontWeight: 'bold',
+        fontSize: 15,
+        color: '#3FB047'
+    },
+    textoPokemon1: {
+        textAlign: 'center',
+        marginTop: 20,
+        color: '#9390AC'
+    },
+    textoPokemon2: {
+        textAlign: 'center',
+        color: '#9390AC'
+    }
 
 })
